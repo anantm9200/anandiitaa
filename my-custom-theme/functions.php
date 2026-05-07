46b21b298ca0c94c8b4f9c72745f8f87af183e53
Heramb <heramb@invadecode.com>
2026-05-07 14:23:24 +0530

    Remove the extra line in health benefits of Jaggery

--- my-custom-theme/functions.php
+++ my-custom-theme/functions.php
@@ -9,7 +9,7 @@ function anandiitaa_enqueue_assets() {
     );
 
     // Theme stylesheet (self-hosts AppetitePro + DM Sans via @font-face)
-    wp_enqueue_style( 'main-styles', get_stylesheet_uri(), array( 'google-fonts-montserrat' ), '8.5' );
+    wp_enqueue_style( 'main-styles', get_stylesheet_uri(), array( 'google-fonts-montserrat' ), '8.6' );
 
     // Hero carousel script
     wp_enqueue_script(
