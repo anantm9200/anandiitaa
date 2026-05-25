0fa28f9068b899670f183c7f8f478f555303f2a6
Heramb <heramb@invadecode.com>
2026-05-25 17:41:49 +0530

    1280 homepage change 1

--- my-custom-theme/service-worker.js
+++ my-custom-theme/service-worker.js
@@ -25,7 +25,7 @@
  * delete any caches whose name doesn't match the current version.
  */
 
-const CACHE_VERSION = 'v9';
+const CACHE_VERSION = 'v10';
 const HTML_CACHE    = `anandiitaa-html-${CACHE_VERSION}`;
 const ASSET_CACHE   = `anandiitaa-assets-${CACHE_VERSION}`;
 
